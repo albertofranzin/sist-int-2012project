785bcc01b8b0b8c84937520eba5f506ccc140af5
Alberto Franzin <albertofranzin@users.noreply.github.com>
2012-12-05 11:03:24 +0000

    altre prove

--- source/prova.py
+++ source/prova.py
@@ -2,20 +2,83 @@
 
 import ply.lex as lex
 import re
+import os
 import string
+
+from bs4 import BeautifulSoup
+
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
 
 # DEFINE SOME USEFUL THINGS
 id_tab = "".join(map(chr, xrange(256)))
 tostrip = "".join( c for c in id_tab if not ( c.isalnum() or c.isspace() ) )
 # isalnum() == isaplha() or isdigit()
 
+# DEFINE HTML PARSING
+class MyHTMLParser(HTMLParser):
+
+  def handle_starttag(self, tag, attrs):
+    print "Start tag:", tag
+    for attr in attrs:
+      print "     attr:", attr
+
+  def handle_endtag(self, tag):
+    print "End tag  :", tag
+
+  def handle_data(self, data):
+    print "Data     :", data
+
+  def handle_comment(self, data):
+    print "Comment  :", data
+
+  def handle_entityref(self, name):
+    c = unichr(name2codepoint[name])
+    #c = unichr(name)
+    print "Named ent:", c
+
+  def handle_charref(self, name):
+    if name.startswith('x'):
+      c = unichr(int(name[1:], 16))
+    else:
+      c = unichr(int(name))
+    print "Num ent  :", c
+
+  def handle_decl(self, data):
+    print "Decl     :", data
+
+parser = MyHTMLParser()
+
+parser.feed('<html><head><title>Ciao</title></head><body>AAAAAJHGJHGJ<a href="http://ciao.com">akshdjkshad</a></body></html>')
+
 # DEFINE LEX RULES
 
 tokens = ( 'ALLCAPSW',
            'ALPHANUM', # also known as 'bimbominkia style'
+           'LINKADDR',
+           'HTMLTAG',
+           'MAILADDR',
            'LOWERCASEW',
            'TITLEW'
          )
+
+def t_MAILADDR(token):
+  r'[a-zA-Z0-9]+\@(?:[a-zA-Z0-9]+\.)+[a-zA-Z]+'
+  token.value = token.value.lower()
+  token.type = 'MAILADDR'
+  return token
+
+def t_LINK(token):
+  r'http:\/\/[a-zA-Z0-9]+(?:[a-zA-Z0-9]+\.)+[a-zA-Z]+[\/[a-zA-Z0-9\?\-_\.\=\%\#]+]*'
+  token.value = token.value.lower()
+  token.type = 'LINKADDR'
+  return token
+
+def t_HTMLTAG(token):
+  r'\<'
+  token.value = token.value.lower()
+  token.type = 'HTMLTAG'
+  return token
 
 def t_ALPHANUM(token):
   r'[a-zA-Z0-9]*[0-9]+[a-zA-Z0-9]+'
@@ -55,6 +118,12 @@ def t_error(t):
 #  r = []
 #  return s
 
+# PROCESS TOKENS
+
+def process_tokens(result):
+  for token in result:
+    (type, value) = (token.type, token.value)
+
 # CREATE LEXER
 
 lexer = lex.lex()
@@ -73,12 +142,27 @@ def lexer_words(text):
 
 # TEST
 
-mail = """This is a s.p.a.m. m41l, ju
-st F0r EXAMPLE"""
+#mail = """This is a s.p.a.m. m41l, ju
+#st F0r EXAMPLE"""
+
+#in_file = open("spam.txt", "r")
+#mail = in_file.read()
+#in_file.close()
 
 #print valid_words(mail)
 
-print lexer_words(mail.translate(id_tab, tostrip))
+#print lexer_words(mail.translate(id_tab, tostrip))
+
+
+os.chdir("spam/hard_ham/")
+for files in os.listdir("."):
+  in_file = open(files, "r")
+  mail = in_file.read()
+  in_file.close()
+  print "\n\n\n#############  ", files, "     ########\n\n\n"
+  #soup = BeautifulSoup(mail, convertEntities=BeautifulSoup.HTML_ENTITIES)
+  parser.feed(mail)
+  #print lexer_words(mail)#(mail.translate(id_tab, tostrip))
 
 
 ###############################
