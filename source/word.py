8a27c0b2fc671f039d7f4a192e47d94f4ffb303c
Alberto Franzin <albertofranzin@users.noreply.github.com>
2012-12-07 13:02:44 +0000

    sistemata un po' la forma

--- source/word.py
+++ source/word.py
@@ -2,7 +2,9 @@
 
 
 class Word:
-    "Stats for a single word: how many times this word appears in a spam mail, and how many times it appears in a ham mail"
+    """Stats for a single word: how many times this word appears in a spam mail, and how many times it appears in a ham mail"""
+
     def __init__(self, spam_occurrences, ham_occurrences):
+        """constructor"""
         self.ham_occurrences = ham_occurrences
         self.spam_occurrences = spam_occurrences
