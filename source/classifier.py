aace4ebab569565ed0d60d310e73091b29d021c9
alberto.franzin@gmail.com <alberto.franzin@gmail.com@f60a5ab7-9378-e921-1f98-cde1e1f84c8f>
2012-12-18 13:05:23 +0000

    corretto un errore grave...

--- source/classifier.py
+++ source/classifier.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Classifier:
     """Classify the test set.
 
@@ -5,95 +8,8 @@ class Classifier:
 
     """
 
-    # def __init__(self):
-    #     pass
-
-    # @staticmethod
-    # def diffs(a, b, c, d):
-    #     pass
-
-    # @staticmethod
-    # def classify(mws, mgs, words, general_stats, config):
-    #     """
-    #     TODO: INSERT DOCUMENTATION HERE!!!
-    #     """
-
-    #     print "Classifier :: classify :: begin"
-    #     # prob = 1.0
-    #     word_list = mws.keys()
-    #     ovrl_list = words.keys()
-    #     overall_pspam = 1.0
-    #     overall_pham = 1.0
-    #     print "Classifier :: classify :: loop starts now"
-    #     for word in word_list:
-    #         if word in ovrl_list:
-    #             pspam = words[word].spam_occurrences
-    #             pham  = words[word].ham_occurrences
-    #             ptot = pspam + pham
-    #             # print "word '", word, "' stats : ", pspam, "times spam,", pham, "times ham, ", ptot, "total",
-    #             # print pspam / (ptot * 1.0),
-    #             # prob *= (1.0 * pspam + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-    #             # print "  prob  ", (1.0 * pspam + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-
-    #             prob_spam = (1.0 * pspam + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-    #             prob_ham = (1.0 * pham + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-    #             if prob_spam > prob_ham:
-    #                 print "-- ", word, " is more likely to be spam",
-    #                 print prob_spam / (prob_spam + prob_ham)
-    #             else:
-    #                 print "-- ", word, " is more likely to be ham",
-    #                 print prob_ham / (prob_spam + prob_ham)
-
-    #             overall_pspam *= (1.0 * pspam + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-    #             overall_pham *= (1.0 * pham + config.SMOOTH_VALUE) / (1.0 * ptot + config.SMOOTH_VALUE * 2.0)
-
-    #         else:
-    #             # prob *= 1
-    #             #print "--  word '", word, "' never met before"
-    #             # overall_pspam *= 0.9
-    #             pass
-    #         # print word, prob
-
-    #     # use the overall features stats
-    #     prob_gen_spam = 1.0
-    #     prob_gen_ham = 1.0
-    #     stats = general_stats.keys()
-    #     for stat_id in stats:
-    #         stat = mgs[stat_id]
-    #         prob_gen_ham *= (1.0 * stat.ham + config.SMOOTH_VALUE) / (1.0 * (stat.spam + stat.ham) + 2.0 * config.SMOOTH_VALUE)
-    #         prob_gen_spam *= (1.0 * stat.spam + config.SMOOTH_VALUE) / (1.0 * (stat.spam + stat.ham) + 2.0 * config.SMOOTH_VALUE)
-
-    #     print "Classifier :: classify :: done : ", overall_pspam, overall_pham,
-    #     print prob_gen_spam, prob_gen_ham
-
-    #     final_stats = prob_gen_spam / (prob_gen_spam + prob_gen_ham)
-    #     final_words = overall_pspam / (overall_pham + overall_pspam)
-
-    #     # if overall_pham >= overall_pspam:
-    #     print final_words, final_stats
-    #     print final_stats * config.OVERALL_FEATS_SPAM_W + final_words * (1 - config.OVERALL_FEATS_SPAM_W)
-
-    #     overall_pspam = overall_pspam * (1 - config.OVERALL_FEATS_SPAM_W) + prob_gen_spam * config.OVERALL_FEATS_SPAM_W
-    #     overall_pham = overall_pham * (1 - config.OVERALL_FEATS_SPAM_W) + prob_gen_ham * config.OVERALL_FEATS_SPAM_W
-    #     if overall_pspam / (overall_pham + overall_pspam) > config.SPAM_THR:
-    #         print "####################################"
-    #         print "#                                  #"
-    #         print "#  mail is more likely to be spam  #"
-    #         print "#                                  #"
-    #         print "####################################"
-    #         decision = True
-    #     else:
-    #         print "###################################"
-    #         print "#                                 #"
-    #         print "#  mail is more likely to be ham  #"
-    #         print "#                                 #"
-    #         print "###################################"
-    #         decision = False
-
-    #     return decision
-
     @staticmethod
-    def classify(ws, gs, ovrl_ws, ovrl_gs):
+    def classify(ws, gs, ovrl_ws, ovrl_gs, config):
         """
         Classification function which guesses the class of a mail. The Bayesian logic\
         is applied here.
@@ -111,33 +27,36 @@ class Classifier:
         :type ws: array of :class:`test_stat.Test_word` objects
         :param gs: array containing the features encontered in the mail;
         :type gs: array of :class:`test_stat.Test_stat` objects
+        :param config: contains some general parameters and configurations;
+        :type config: :class:`config.Config` object
         :return: `True` if the mail is classified as spam, `False` if it is\
             considered ham.
 
         """
 
-        ovrl_words = self.words.keys()
-        smth = self.config.SMOOTH_VALUE
+        ovrl_words = ovrl_ws.keys()
+        smth = config.SMOOTH_VALUE
         ovrl_pspam = 1.0
         ovrl_pham = 1.0
         for word in ws.keys():
             word_count = ws[word].occurrences
             if word in ovrl_words:
                 # already met
-                s_occ = ovrl_ws[word].spam_occurrences / 1.0 / self.config.SIZE_OF_BAGS
-                h_occ = ovrl_ws[word].ham_occurrences / 1.0 / self.config.SIZE_OF_BAGS
+                s_occ = ovrl_ws[word].spam_occurrences / 1.0 / config.SIZE_OF_BAGS
+                h_occ = ovrl_ws[word].ham_occurrences / 1.0 / config.SIZE_OF_BAGS
                 t_occ = s_occ + h_occ
                 pws = ((s_occ + smth) / (t_occ + 2.0 * smth)) ** word_count
                 pwh = ((h_occ + smth) / (t_occ + 2.0 * smth)) ** word_count
-                if pws > pwh:
-                    print "-- ", word, " is more likely to be spam",
-                    if pws + pwh > 0:
-                        print 1.0 * pws / (1.0 * pws + 1.0 * pwh),
-                else:
-                    print "-- ", word, " is more likely to be ham",
-                    if pws + pwh > 0:
-                        print 1.0 * pwh / (1.0 * pws + 1.0 * pwh),
-                print word_count, s_occ, h_occ
+                if config.VERBOSE:
+                    if pws > pwh:
+                        print "-- ", word, " is more likely to be spam",
+                        if pws + pwh > 0:
+                            print 1.0 * pws / (1.0 * pws + 1.0 * pwh),
+                    else:
+                        print "-- ", word, " is more likely to be ham",
+                        if pws + pwh > 0:
+                            print 1.0 * pwh / (1.0 * pws + 1.0 * pwh),
+                    print word_count, s_occ, h_occ
 
                 ovrl_pspam *= pws
                 ovrl_pham *= pwh
@@ -151,12 +70,12 @@ class Classifier:
         stats = gs.keys()
         for stat_id in stats:
             stat = gs[stat_id].count
-            ds = math.fabs(ovrl_gs[stat_id].spam / 1.0 / self.config.SIZE_OF_BAGS - stat)
-            dh = math.fabs(ovrl_gs[stat_id].ham / 1.0 / self.config.SIZE_OF_BAGS - stat)
+            ds = math.fabs(ovrl_gs[stat_id].spam / 1.0 / config.SIZE_OF_BAGS - stat)
+            dh = math.fabs(ovrl_gs[stat_id].ham / 1.0 / config.SIZE_OF_BAGS - stat)
             prob_gen_spam *= (dh + smth) / (ds + dh + 2.0 * smth)
             prob_gen_ham *= (ds + smth) / (ds + dh + 2.0 * smth)
 
-            if self.config.VERBOSE:
+            if config.VERBOSE:
                 if ds < dh:
                     print "-- ", gs[stat_id].description, " is more likely to be spam",
                     if ds + dh > 0:
@@ -165,14 +84,15 @@ class Classifier:
                     print "-- ", gs[stat_id].description, " is more likely to be ham",
                     if ds + dh > 0:
                         print 1.0 * ds / (1.0 * ds + dh), 1.0 * dh / (1.0 * ds + dh),
-                print stat, ovrl_gs[stat_id].spam / 1.0 / self.config.SIZE_OF_BAGS,
-                print ovrl_gs[stat_id].ham / 1.0 / self.config.SIZE_OF_BAGS
+                print stat, ovrl_gs[stat_id].spam * 1.0 / config.SIZE_OF_BAGS,
+                print ovrl_gs[stat_id].ham * 1.0 / config.SIZE_OF_BAGS
 
-        final_prob_spam = prob_gen_spam * self.config.OVERALL_FEATS_SPAM_W + ovrl_pspam * (1 - self.config.OVERALL_FEATS_SPAM_W)
-        final_prob_ham = prob_gen_ham * self.config.OVERALL_FEATS_SPAM_W + ovrl_pham * (1 - self.config.OVERALL_FEATS_SPAM_W)
-        print "** **", final_prob_spam, final_prob_ham, "** **"
+        final_prob_spam = 1.0 * prob_gen_spam * config.OVERALL_FEATS_SPAM_W + ovrl_pspam * 1.0 * (1 - config.OVERALL_FEATS_SPAM_W)
+        final_prob_ham = 1.0 * prob_gen_ham * config.OVERALL_FEATS_SPAM_W + ovrl_pham * 1.0 * (1 - config.OVERALL_FEATS_SPAM_W)
+        if config.VERBOSE:
+            print "** **", final_prob_spam, final_prob_ham, "** **"
         #if final_prob_spam > final_prob_ham and
-        if (final_prob_spam / (final_prob_spam + final_prob_ham)) >= self.config.SPAM_THR:
+        if (final_prob_spam / (final_prob_spam + final_prob_ham)) >= config.SPAM_THR:
             print "Mail is more likely to be spam",
             ret_val = True
         else:
