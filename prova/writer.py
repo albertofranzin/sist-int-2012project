b7202e3e5ccf251b0911960e5f42cc8419ccf41b
Alberto Franzin <albertofranzin@users.noreply.github.com>
2012-12-26 16:40:45 +0000

    aggiornata relazione

--- prova/writer.py
+++ prova/writer.py
@@ -6,13 +6,11 @@ prova = Word(spam_count,ham_count)
 # print prova.ham_occurrences
 
 with open('prova.csv', 'wb') as csvfile:
-	spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	for count in range(1,spam_count)
-		spamwriter.writerow(['Spam', 'Spam', 'Spam'])
-	spamwriter.writerow([prova])
-	spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
+    spamwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    spamwriter.writerow([prova])
+    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
 
 with open('prova.csv', 'rb') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=',')
-	for row in spamreader:
-		print ', '.join(row)
+    spamreader = csv.reader(csvfile, delimiter=',')
+    for row in spamreader:
+        print ', '.join(row)
